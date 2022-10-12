4dede7244bb8c252ef4132dcb42dd26b1cabf97d
varmesokker <94365938+varmesokker@users.noreply.github.com>
2022-10-12 08:56:37 +0200

    Add files via upload
    Script for reading and writing to/from database

---
+++ Teste Python til Database/GetFromDatabase_pymysql.py
@@ -0,0 +1,30 @@
+import pymysql
+
+# Connection to database
+database_SLS = "SpiceLogisticsSystems"
+db_user = "root"
+db_pwd = "password"
+php_host = "localhost/phpmyadmin/"
+php_port = 80
+
+connection = pymysql.connect(
+    host="localhost",
+    port=3307,
+    user="root",
+    passwd="password",
+    database="SpiceLogisticsSystems"
+)
+
+
+
+with connection:
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `Student` WHERE `Studnr`=125"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
+
+
+
+
+
