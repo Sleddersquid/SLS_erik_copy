b6a0461245a8a35c621df571f7c4c5ed3fe16706
Aditi Deshpande <aditirdeshpande28@gmail.com>
2022-10-19 09:51:37 +0200

    changes

--- flaskblog.py
+++ flaskblog.py
@@ -7,19 +7,13 @@ click on the website shown
 @author: aditi
 """
 
-from flask import Flask, render_template,Response
-from pyzbar.pyzbar import decode
-import cv2
+from flask import Flask, render_template,url_for
 #from pyzbar.pyzbar import decode
 #instantiated flask application
-app = Flask(__name__)
-app = Flask(__name__, template_folder='Template')
-cap=cv2.VideoCapture(0)
-cap.set(3, 640) # 3 -width
-cap.set(4, 480) # 4 -Height
+app = Flask(__name__,template_folder='Template')
 
 
-Index = [
+posts = [
     {'Creators' : 'Aditi Deshpande, Theo Magnor',
      'title' : 'Post 1',
      'content' : 'lorem ipsum',
@@ -33,39 +27,7 @@ Index = [
      }
     ]
 
-def generate_frames():
-    camera=True
-    while camera == True:
-        success, frame = cap.read()
-
-        for code in decode(frame):
-            print(code.type)
-            print(code.data.decode)
-
-        cv2.imshow("scan",frame)
-        cv2.waitKey(0)
-    '''
-    while True:
-            
-        ## read the camera frame
-        success,frame=camera.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    '''
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    
 
 
 
@@ -76,20 +38,20 @@ def video():
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',Index=Index)
+    return render_template('home.html',posts=posts)
 
 
 
 #about page
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 
 
 #running the app, debug is true so that we don't need to close web server and refresh to see changes 
 if __name__ == '__main__':
-        app.run(debug=False)
+        app.run(debug=True)
 
 
     
